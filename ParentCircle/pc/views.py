ea6c8ef6e4b4db6940206974591d20f44105b70b
Naveen N <naveen.nagarajan@seaant.com>
2016-03-21 14:44:21 +0530

    session text changed

--- ParentCircle/pc/views.py
+++ ParentCircle/pc/views.py
@@ -14,7 +14,7 @@ def landing_page(request):
     return render_to_response('landing_page.html',locals(),context_instance = RequestContext(request))
 
 def home(request):
-    request.session['session_check'] = "This is from session"
+    request.session['session_check'] = "This is from new session text, after 2.45pm"
     return render_to_response('home.html',locals(),context_instance = RequestContext(request))
 
 def remove_session(request):
