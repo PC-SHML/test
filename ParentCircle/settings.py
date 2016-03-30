465e068c0fee98ca51a9345f4ee7ea6cd1efd669
PC-SHML <PC-SHML@users.noreply.github.com>
2016-03-30 16:00:16 +0530

    Update settings.py

--- ParentCircle/settings.py
+++ ParentCircle/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'r*+h27lt4iqoo=)_q#6cl_cpeg6#sg*)6u*kap-xywj=$*4)&6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+#SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 SESSION_COOKIE_DOMAIN = 'parentcircle.com'
 SESSION_COOKIE_HTTPONLY ='True'
 ALLOWED_HOSTS = []
