52420541d457546a691952a9eefd8e0304187f7a
PC-SHML <PC-SHML@users.noreply.github.com>
2016-03-30 15:02:12 +0530

    Update views.py

--- ParentCircle/pc/views.py
+++ ParentCircle/pc/views.py
@@ -14,8 +14,16 @@ def landing_page(request):
     return render_to_response('landing_page.html',locals(),context_instance = RequestContext(request))
 
 def home(request):
-    request.session['session_check'] = "This is from new session text, after 2.45pm"
-    return render_to_response('home.html',locals(),context_instance = RequestContext(request))
+    if request.session.test_cookie_worked():
+        request.session.delete_test_cookie()
+        'return HttpResponse("You're logged in.")
+        request.session.set_test_cookie()
+        request.session['session_check'] = "This is from new session text, after 2.45pm"
+        return render_to_response('home.html',locals(),context_instance = RequestContext(request))
+    else:
+        return HttpResponse("Please enable cookies and try again.")
+    
+    
 
 def remove_session(request):
     if 'session_check' in request.session: 
